b5514416b842f0e32a8643ac606798083b93ad3d
jkovacevic992 <jkovacevic992@gmail.com>
2019-02-13 10:10:41 +0100

    Added tags.js and button to toggle tagForm

--- public/js/post_likes.js
+++ public/js/post_likes.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function f() {
     $('#post-likes').on('click', function (e) {
         e.preventDefault();
         var link = $(e.currentTarget);
---
+++ public/js/tags.js
@@ -0,0 +1,7 @@
+$(document).ready(function f() {
+   $('#show_tags').on('click', function (e) {
+      e.preventDefault();
+
+      $('#enter_tags').toggle();
+   });
+});
